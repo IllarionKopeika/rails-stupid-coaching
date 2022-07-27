5da911f7c66117c7a5b1bfbb30be160a0d8f43d4
Illarion Kopeika <larrypearson@yandex.ru>
2022-07-27 15:46:16 +0800

    Add stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,15 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @question = params[:question]
+    if @question.downcase == 'i am going to work right now!'
+      @answer = 'Great, see ya!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I do not care, get dressed and go to work!'
+    end
+  end
 end
